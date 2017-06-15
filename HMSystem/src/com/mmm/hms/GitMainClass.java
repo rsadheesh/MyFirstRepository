94e80047b0d9b4ed9678d21c71a8857d8d17a5ca
rsadheesh <sadheeshjune27@gmail.com>
2017-06-15 15:01:52 +0530

    first commit usr 1

--- HMSystem/src/com/mmm/hms/GitMainClass.java
+++ HMSystem/src/com/mmm/hms/GitMainClass.java
@@ -5,6 +5,7 @@ public class GitMainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test Here!!!");
+		System.out.println("Second");
 	}
 
 }
