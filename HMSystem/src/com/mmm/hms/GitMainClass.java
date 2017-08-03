bf3c1ae2a842cdffc4e10e8e6994d6b41bbfb3aa
sadheesh <sramasamy@mmm.com>
2017-08-03 04:38:37 -0500

    test

--- HMSystem/src/com/mmm/hms/GitMainClass.java
+++ HMSystem/src/com/mmm/hms/GitMainClass.java
@@ -4,11 +4,11 @@ public class GitMainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test Here!!!");
+		System.out.println("Test Here Remote!!!");
 
 		System.out.println("Second usr2 ddd user2 user1 ");
 
-		System.out.println("Second usr2 ddd user2 user222333");
+		System.out.println("Second usr2 ddd user2 user333");
 
 	}
 
