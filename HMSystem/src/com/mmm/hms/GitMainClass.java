6e73aab69d2164855e4d71a866b63e86127a3bf1
sadheesh <sramasamy@mmm.com>
2017-08-03 07:22:20 -0500

    Local commit

--- HMSystem/src/com/mmm/hms/GitMainClass.java
+++ HMSystem/src/com/mmm/hms/GitMainClass.java
@@ -5,9 +5,9 @@ public class GitMainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Test Here Remote wlc test 3!!!");
+		System.out.println("Test Here Remote wlc test 4!!!");
 
-		System.out.println("Second usr2 ddd user2 user1 sadheesh  yousuf shilpa pravin");
+		System.out.println("Second usr2 ddd user2 user1 sadheesh  yousuf shilpa pravin sss");
 
 		System.out.println("Second usr2 ddd user2 user333");
 
