6ce47b1f245a0a7f5d3870dd5d70e5c122121df7
sadheesh <sramasamy@mmm.com>
2017-08-03 05:14:50 -0500

    wlc

--- HMSystem/src/com/mmm/hms/GitMainClass.java
+++ HMSystem/src/com/mmm/hms/GitMainClass.java
@@ -4,7 +4,7 @@ public class GitMainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test Here!!!");
+		System.out.println("Test Here Remote wlc!!!");
 
 		System.out.println("Second usr2 ddd user2 user1 ");
 
